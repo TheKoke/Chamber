3f3f2d5b4b432c99fb995c2e40388b1aba23ed6d
TheKoke <damir.isaev.15@gmail.com>
2026-01-09 16:59:46 +0500

    New UI-s and reimplemented starting position

--- src/backend/start.py
+++ src/backend/start.py
@@ -1,5 +1,5 @@
 from backend.geometry import Geometry
-from backend.environment import Collimator, Target
+from backend.environment import Chamber, AdditionalVolume, CollimationTube, Collimator, Target, Telescope
 
 
 def starting_position() -> Geometry:
@@ -15,15 +15,31 @@ def starting_position() -> Geometry:
     geometry: ``Geometry``
         geometry of LLENR scattering chamber.
     '''
-    first_collimator = Collimator(3.0, 2.0)
-    second_collimator = Collimator(3.0, 2.0, x=600.0)
+    first_collimator = Collimator(3.0, 2.0, x=0.0)
+    second_collimator = Collimator(3.0, 2.0, x=601.0)
+    collimation = CollimationTube(first_collimator, second_collimator)
 
-    target = Target(15.0, 15.0, x=1320.0)
+    target = Target(15.0, 15.0, x=1368.0)
 
-    # detector = Detector(x=1540.0)
+    near_collimator1 = Collimator(4.9, 1.0, x=1448.0)
+    near_collimator2 = Collimator(4.9, 1.0, x=1638.0)
+    near_telescope = Telescope(near_collimator1, near_collimator2)
 
-    geometry = Geometry([first_collimator, second_collimator], target)
-    return geometry
+    rear_collimator1 = Collimator(3.0, 1.0, x=2068.0)
+    rear_collimator2 = Collimator(3.0, 1.0, x=2318.0)
+    rear_telescope = Telescope(rear_collimator1, rear_collimator2)
+
+    points = [
+        [  320.087, -90.0  ],
+        [ 1040.087, -90.0  ],
+        [ 1040.087,  90.0  ],
+        [  815.812, 491.622],
+        [  285.756,  170   ]
+    ]
+    back = AdditionalVolume(points)
+
+    chamber = Chamber(665.0, back, collimation, target, [near_telescope, rear_telescope])
+    return Geometry(chamber)
 
 
 if __name__ == '__main__':
